e856560cd0639e86045355b085bf38c6843e01dc
Yuya Watari <puropuro203@hotmail.com>
2017-03-23 07:38:07 +0900

    Implement roop optimization

--- Brainfuck.Core/Optimizer.cs
+++ Brainfuck.Core/Optimizer.cs
@@ -12,8 +12,105 @@ namespace Brainfuck.Core
         public static Module Optimize(this Module module)
         {
             IReadOnlyList<IOperation> operations = module.Root.Operations;
+            operations = OptimizeRoopStep(operations);
             operations = OptimizeReduceStep(operations);
             return new Module(module.Source, new BlockUnitOperation(operations.ToImmutableArray(), module.Root.PtrChange));
+        }
+
+        private static IReadOnlyList<IOperation> OptimizeRoopStep(IReadOnlyList<IOperation> operations)
+        {
+            var list = new List<IOperation>();
+
+            foreach (var op in operations)
+            {
+                if (op is IUnitOperation unit)
+                {
+                    IReadOnlyList<IOperation> optimized = null;
+                    if (op is RoopUnitOperation roop)
+                    {
+                        optimized = TryOptimizeRoop(roop);
+                        if (optimized != null)
+                        {
+                            list.AddRange(optimized);
+                        }
+                    }
+
+                    if (optimized == null)
+                    {
+                        // Recursive optimize
+                        IUnitOperation newUnit = unit.WithOperations(OptimizeRoopStep(unit.Operations).ToImmutableArray());
+                        list.Add(newUnit);
+                    }
+                }
+                else
+                {
+                    list.Add(op);
+                }
+            }
+
+            return list;
+        }
+
+        private static IReadOnlyList<IOperation> TryOptimizeRoop(RoopUnitOperation roop)
+        {
+            if (roop.PtrChange != 0)
+            {
+                // If this roop changes ptr, we can't optimize this
+                return null;
+            }
+
+            int origin = roop.Src.Offset;
+            var deltas = new Dictionary<int, int>() { [origin] = 0 };
+
+            void EnsureKey(int key)
+            {
+                if (!deltas.ContainsKey(key))
+                    deltas[key] = 0;
+            }
+
+            foreach (var op in roop.Operations)
+            {
+                if (op is AddAssignOperation add)
+                {
+                    EnsureKey(add.Dest.Offset);
+                    deltas[add.Dest.Offset] += add.Value;
+                }
+                else
+                {
+                    // We can't optimize this roop
+                    return null;
+                }
+            }
+
+            if (deltas[origin] != -1)
+            {
+                return null;
+            }
+
+            var optimized = new List<IOperation>();
+
+            foreach (var p in deltas.Where(p => p.Key != origin))
+            {
+                // buffer[ptr + p.Key] += buffer[ptr] * p.Value
+                optimized.Add(new MultAddAssignOperation(new MemoryLocation(p.Key), new MemoryLocation(origin), p.Value));
+            }
+
+            // buffer[ptr] = 0
+            optimized.Add(new AssignOperation(new MemoryLocation(origin), 0));
+
+            if (deltas.Where(p => p.Key < origin).Any())
+            {
+                // There are some accesses to buffer[ptr + x] where x < 0.
+                // Such accesses sometimes cause out of range access.
+                // So we have to make sure that buffer[ptr] != 0.
+
+                // if (buffer[ptr] == 0) { goto `]`; }
+                var iftrue = new IfTrueUnitOperation(optimized.ToImmutableArray(), 0, new MemoryLocation(origin));
+                optimized.Clear();
+                optimized.Add(iftrue);
+            }
+
+            return optimized;
         }
 
         private static IReadOnlyList<IOperation> OptimizeReduceStep(IReadOnlyList<IOperation> operations)
@@ -95,8 +192,6 @@ namespace Brainfuck.Core
 
         private static IOperation TryReduce(IOperation a, IOperation b)
         {
-            var type = (a.GetType(), b.GetType());
-
             if (a is IWriteOperation wa && b is IWriteOperation wb && wa.Dest == wb.Dest)
             {
                 MemoryLocation dest = wa.Dest;
@@ -154,66 +249,7 @@ namespace Brainfuck.Core
             return ImmutableArray.CreateRange(list);
         }
 
-        private static IReadOnlyList<IOperation> TryOptimizeRoop(Roop roop)
-        {
-            var deltas = new Dictionary<int, int>() { [0] = 0 };
-            int offset = 0;
-
-            void EnsureKey(int key)
-            {
-                if (!deltas.ContainsKey(key))
-                    deltas[key] = 0;
-            }
-
-            foreach (var op in roop.Operations)
-            {
-                if (op is AddPtr addptr)
-                {
-                    offset += addptr.Value;
-                }
-                else if (op is AddValue addvalue)
-                {
-                    int dest = offset + addvalue.Dest.Offset;
-                    EnsureKey(dest);
-                    deltas[dest] += addvalue.Value;
-                }
-                else
-                {
-                    // We can't optimize this roop
-                    return null;
-                }
-            }
-
-            if (offset != 0 || deltas[0] != -1)
-            {
-                return null;
-            }
-
-            var optimized = new List<IOperation>();
-
-            foreach (var p in deltas.Where(p => p.Key != 0))
-            {
-                // buffer[ptr + p.Key] += buffer[ptr] * p.Value
-                optimized.Add(new MultAdd(new MemoryLocation(p.Key), MemoryLocation.Zero, p.Value));
-            }
-
-            // buffer[ptr] = 0
-            optimized.Add(new Assign(MemoryLocation.Zero, 0));
-
-            if (deltas.Where(p => p.Key < 0).Any())
-            {
-                // There are some accesses to buffer[ptr + x] where x < 0.
-                // Such accesses sometimes cause out of range access.
-                // So we have to make sure that buffer[ptr] != 0.
-
-                // if (buffer[ptr] == 0) { goto `]`; }
-                IfTrue iftrue = new IfTrue(MemoryLocation.Zero, ImmutableArray.CreateRange(optimized));
-                optimized.Clear();
-                optimized.Add(iftrue);
-            }
-
-            return optimized;
-        }
+        
 
         private static ImmutableArray<IOperation> OptimizeReduceStep(IReadOnlyList<IOperation> operations, int offset, bool updateOffsetAtLast)
         {
