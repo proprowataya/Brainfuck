697861786726bf10bce55fe60c880a0f8ee9f5c1
Yuya Watari <puropuro203@hotmail.com>
2017-03-22 17:15:41 +0900

    Rewrite test code

--- Brainfuck.Test/ExecutionTest.cs
+++ Brainfuck.Test/ExecutionTest.cs
@@ -2,48 +2,46 @@ using System;
 using System.IO;
 using Xunit;
 using Brainfuck.Core;
+using System.Collections.Generic;
 
 namespace Brainfuck.Test
 {
     public class ExecutionTest
     {
-        private const int DefaultBufferSize = 1;
-        private static readonly Type[] TestTypes = { typeof(Byte), typeof(Int16), typeof(Int32), typeof(Int64) };
-
-        [Fact]
-        public void HelloWorldTest()
+        [Theory, MemberData(nameof(ExecutionCase))]
+        public void HelloWorldTest(object description, Action<Program> action, bool optimize)
         {
             const string HelloWorld = "Hello World!";
             const string Code = ">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-]<.>+++++++++++[<+++++>-]<.>++++++++[<+++>-]<.+++.------.--------.[-]>++++++++[<++++>-]<+.[-]++++++++++.";
-            TestAll(Code, HelloWorld);
+            Assert.Equal(HelloWorld, Test(action, Code, optimize));
         }
 
-        [Fact]
-        public void HelloWorldWithErrorsTest()
+        [Theory, MemberData(nameof(ExecutionCase))]
+        public void HelloWorldWithErrorsTest(object description, Action<Program> action, bool optimize)
         {
             const string HelloWorld = "Hello World!";
             const string Code = ">++++  + + + ++111111[<1+1+1+1+1+1+1+1+1>1-1]1<1.1>1+1+1+++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-]<.>+++++++++++[<+++++>-]<.>++++++++[<+++>-]<.+++.------.--------.[-]>++++++++[<++++>-]<+.[-]+++++++++11+.";
-            TestAll(Code, HelloWorld);
+            Assert.Equal(HelloWorld, Test(action, Code, optimize));
         }
 
-        [Fact]
-        public void EchoTest()
+        [Theory, MemberData(nameof(ExecutionCase))]
+        public void EchoTest(object description, Action<Program> action, bool optimize)
         {
             const string InputString = "This is a test string.\0";
             const string Code = "+[,.]";
-            TestAll(Code, InputString, InputString);
+            Assert.Equal(InputString, Test(action, Code, optimize, InputString));
         }
 
-        [Fact]
-        public void EchoTest2()
+        [Theory, MemberData(nameof(ExecutionCase))]
+        public void EchoTest2(object description, Action<Program> action, bool optimize)
         {
             const string InputString = "This is a test string.\0";
             const string Code = ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>+[,.]";
-            TestAll(Code, InputString, InputString);
+            Assert.Equal(InputString, Test(action, Code, optimize, InputString));
         }
 
-        [Fact]
-        public void PrimeTest()
+        [Theory, MemberData(nameof(ExecutionCase))]
+        public void PrimeTest(object description, Action<Program> action, bool optimize)
         {
             // http://esoteric.sange.fi/brainfuck/bf-source/prog/PRIME.BF
             const string Code = @"
@@ -64,31 +62,17 @@ namespace Brainfuck.Test
 [-]<<-]++++++++++.[-]@";
             const string InputString = "25\n";
             const string Expected = "Primes up to: 2 3 5 7 11 13 17 19 23";
-            TestAll(Code, Expected, InputString);
+            Assert.Equal(Expected, Test(action, Code, optimize, InputString));
         }
 
-        private static void TestAll(string code, string expected, string stdin = "")
+        private static string Test(Action<Program> action, string source, bool optimize, string stdin = "")
         {
-            Program[] programs = { Parser.Parse(code), Optimizer.Optimize(Parser.Parse(code)) };
-
-            foreach (var program in programs)
+            Program program = Parser.Parse(source);
+            if (optimize)
             {
-                foreach (var type in TestTypes)
-                {
-                    Assert.Equal(expected, RunTest(GetInterpreterAction(type), program, stdin));
-
-                    foreach (var unsafeCode in new[] { true, false })
-                    {
-                        Setting setting = Setting.Default.WithElementType(type).WithUnsafeCode(unsafeCode);
-                        Assert.Equal(expected, RunTest(GetCompilerAction(setting), program, stdin));
-                        Assert.Equal(expected, RunTest(GetILCompilerAction(setting), program, stdin));
-                    }
-                }
+                program = program.Optimize();
             }
-        }
 
-        private static string RunTest(Action<Program> action, Program program, string stdin)
-        {
             using (var reader = new StringReader(stdin))
             using (var writer = new StringWriter())
             {
@@ -97,6 +81,39 @@ namespace Brainfuck.Test
                 action(program);
                 Console.Out.Flush();
                 return writer.ToString().TrimEnd();
+            }
+        }
+
+        private const int DefaultBufferSize = 1;
+        private static readonly Type[] TestTypes = { typeof(Byte), typeof(Int16), typeof(Int32), typeof(Int64) };
+
+        // (object description, Action<Program> action, bool optimize)
+        public static IEnumerable<object[]> ExecutionCase
+        {
+            get
+            {
+                foreach (var Type in TestTypes)
+                {
+                    foreach (var Optimize in new[] { false, true })
+                    {
+                        yield return new object[]
+                        {
+                            new { Type, Optimize },
+                            GetInterpreterAction(Type),
+                            Optimize
+                        };
+
+                        foreach (var Unsafe in new[] { false, true })
+                        {
+                            yield return new object[]
+                            {
+                                new { Type, Optimize, Unsafe },
+                                GetILCompilerAction(Setting.Default.WithElementType(Type).WithUnsafeCode(Unsafe)),
+                                Optimize
+                            };
+                        }
+                    }
+                }
             }
         }
 
